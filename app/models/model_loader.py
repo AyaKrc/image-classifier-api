2dc5aad49f5444478d8b7fa1df4e116c1c384352
AyaKrc <karbich.aya.2003@gmail.com>
2025-09-07 21:47:02 +0100

    Update model loader to fetch weights from Hugging Face Hub

--- app/models/model_loader.py
+++ app/models/model_loader.py
@@ -1,16 +1,34 @@
 import torch
 import timm
 import json
-import os
+from huggingface_hub import hf_hub_download
 
+# Device setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# load labels
+# Load labels (these stay local, small file)
 with open("artifacts/labels.json", "r") as f:
     CLASS_NAMES = json.load(f)
 
 def load_model():
-    model = timm.create_model("convnextv2_base.fcmae_ft_in1k", pretrained=False, num_classes=2)
-    model.load_state_dict(torch.load("artifacts/best_model.pth", map_location=device))
+    # Download model weights from Hugging Face Hub
+    model_path = hf_hub_download(
+        repo_id="AyaKrc/image-classifier-cifake",  # your HF repo
+        filename="best_model.pth"
+    )
+
+    # Recreate architecture
+    model = timm.create_model(
+        "convnextv2_base.fcmae_ft_in1k",
+        pretrained=False,
+        num_classes=2
+    )
+
+    # Load weights
+    state_dict = torch.load(model_path, map_location=device)
+    model.load_state_dict(state_dict)
+
+    # Prepare for inference
     model.eval().to(device)
+
     return model, CLASS_NAMES, device
